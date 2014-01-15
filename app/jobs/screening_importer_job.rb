5dd6b3cb22dc164718c40c42cdca3876bfd478a8
Andy Croll <andy@goodscary.com>
2014-01-15 23:39:47 +0000

    extract method for clarity

--- app/jobs/screening_importer_job.rb
+++ app/jobs/screening_importer_job.rb
@@ -1,11 +1,16 @@
 class ScreeningImporterJob < Struct.new(:cinema_id, :film_name, :showing_at, :variant)
   def perform
     cinema = Cinema.find(cinema_id)
-    film = Film.find_or_create_by(name: film_name)
     screening = cinema.screenings.find_or_create_by(
-      film: film,
+      film: film(film_name),
       showing_at: showing_at
     )
     screening.update_attributes(variant: variant, updated_at: Time.current)
+  end
+
+  private
+
+  def film(name)
+    Film.find_or_create_by(name: name)
   end
 end
