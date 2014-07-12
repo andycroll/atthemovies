5ce4301664686c467e73645c49bf1baf98b55755
Andy Croll <andy@goodscary.com>
2014-07-12 23:19:16 +0100

    tidy up screening tests

--- spec/models/screening_spec.rb
+++ spec/models/screening_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe Screening do
   describe 'associations' do
-    it { should belong_to :film }
-    it { should belong_to :cinema }
+    it { is_expected.to belong_to :film }
+    it { is_expected.to belong_to :cinema }
   end
 
   describe 'validations' do
-    it { should validate_presence_of :showing_at }
-    it { should validate_presence_of :variant }
+    it { is_expected.to validate_presence_of :showing_at }
+    it { is_expected.to validate_presence_of :variant }
   end
 
   describe 'scopes' do
@@ -27,14 +27,16 @@ describe Screening do
   end
 
   describe '#set_variant' do
+    subject(:set_variant) { screening.set_variant('my text') }
+
     let(:screening) { create :screening }
-    subject { screening.set_variant('my text') }
 
     it 'changes variant text' do
-      expect { subject }.to change(screening, :variant).to('my text')
+      expect { set_variant }.to change(screening, :variant).to('my text')
     end
+
     it 'updates timestamp' do
-      expect { subject }.to change(screening, :updated_at)
+      expect { set_variant }.to change(screening, :updated_at)
     end
   end
 end
