296368a2b353a4b1a4cd6620c1ba7a1275ad214b
Andy Croll <andy@goodscary.com>
2014-09-24 22:20:13 +0100

    add log rage

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :test do
 end
 
 group :production do
+  gem 'lograge'
   gem 'newrelic_rpm'
   gem 'rails_12factor'
   gem 'unicorn'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Moviesuk::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.lograge.enabled = true
 end
