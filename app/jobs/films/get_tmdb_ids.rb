512c41fca4dc15963b6ae150b973eaba90220cb5
Andy Croll <andy@goodscary.com>
2015-01-01 15:52:36 +0000

    prime the cache for external films

--- app/jobs/films/get_tmdb_ids.rb
+++ app/jobs/films/get_tmdb_ids.rb
@@ -5,12 +5,21 @@ module Films
     def perform(film)
       self.film = film
       film.update_possibles(possible_tmdb_ids)
+      prime_cache
     end
 
     private
 
+    def external_films
+      @external_films ||= ExternalFilm.find(film.name)
+    end
+
     def possible_tmdb_ids
-      Tmdb::Movie.find(film.name).map(&:id)
+      external_films.map(&:tmdb_id)
+    end
+
+    def prime_cache
+      external_films.each { |film| film.title_and_year }
     end
   end
 end
