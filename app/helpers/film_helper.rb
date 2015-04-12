a2c69a9fffa66e3a26156db2c169f725bc81aa3c
Andy Croll <andy@goodscary.com>
2015-04-12 21:36:50 +0100

    add link to tmdb

--- app/helpers/film_helper.rb
+++ app/helpers/film_helper.rb
@@ -1,9 +1,13 @@
 module FilmHelper
-  def tmdb_url(id)
-    "https://www.themoviedb.org/movie/#{id}"
-  end
-
   def imdb_url(id)
     "http://www.imdb.com/title/#{id}"
+  end
+
+  def querify(name)
+    name.downcase.gsub(/[^a-z0-9 ]/, '').gsub(' ', '+')
+  end
+
+  def tmdb_url(id)
+    "https://www.themoviedb.org/movie/#{id}"
   end
 end
