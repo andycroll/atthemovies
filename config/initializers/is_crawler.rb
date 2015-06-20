09a4403ff08de0340a533a5c8eb65181742e6bf3
Andy Croll <andy@goodscary.com>
2015-06-20 23:18:18 +0100

    add dotbot

--- config/initializers/is_crawler.rb
+++ config/initializers/is_crawler.rb
@@ -1,6 +1,7 @@
 Crawler::CUSTOM << Crawler.new(:apple, 'Applebot')
 Crawler::CUSTOM << Crawler.new(:arefs, 'AhrefsBot')
 Crawler::CUSTOM << Crawler.new(:blexbot, 'BLEXBot')
+Crawler::CUSTOM << Crawler.new(:dotbot, 'DotBot')
 Crawler::CUSTOM << Crawler.new(:mailru, 'Mail.RU_Bot')
 Crawler::CUSTOM << Crawler.new(:magestic12, 'MJ12bot')
 Crawler::CUSTOM << Crawler.new(:seznam, 'SeznamBot')
