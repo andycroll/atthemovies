1a59a14c755a94cbeccd2ec9790f38ad9bcbc837
Andy Croll <andy@goodscary.com>
2016-01-30 18:25:50 +0000

    Opbeat in the right place

--- config/initializers/opbeat.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.configure do |config|
-  config.opbeat.app_id = ENV['API_OPBEAT_APP_ID']
-  config.opbeat.organization_id = ENV['API_OPBEAT_ORGANISATION_ID']
-  config.opbeat.secret_token = ENV['API_OPBEAT_SECRET_TOKEN']
-end if Rails.env.production?
