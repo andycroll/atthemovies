84498f292b65fc984b7b5a10f694216418c71aac
Andy Croll <andy@goodscary.com>
2016-01-30 18:19:19 +0000

    New config for opbeat

--- config/initializers/opbeat.rb
+++ config/initializers/opbeat.rb
@@ -1,6 +1,5 @@
-Opbeat.configure do |config|
-  config.organization_id = ENV['API_OPBEAT_ORGANISATION_ID']
-  config.app_id = ENV['API_OPBEAT_APP_ID']
-  config.secret_token = ENV['API_OPBEAT_SECRET_TOKEN']
-  config.async = ->(event) { Thread.new { Opbeat.send(event) } }
+Rails.application.configure do |config|
+  config.opbeat.app_id = ENV['API_OPBEAT_APP_ID']
+  config.opbeat.organization_id = ENV['API_OPBEAT_ORGANISATION_ID']
+  config.opbeat.secret_token = ENV['API_OPBEAT_SECRET_TOKEN']
 end if Rails.env.production?
